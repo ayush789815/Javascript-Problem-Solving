f0156e62905ec78aa2f78ba21c548d53caf4c2c5
ayush789815 <your-email@example.com>
2026-03-11 18:24:48 +0530

    Day 1  Basic Javascript problem solution question

---
+++ script.js
@@ -0,0 +1,103 @@
+//Basic JavaScript Logic Questions===========================
+
+// ===============1. Print "Hello World" in JavaScript=====================
+// console.log("Hello World")
+
+// =====================2. Add two numbers and print the result.=================
+//  let a = 2
+//  let b = 3
+//   console.log(a+b)
+
+// ==================3. Swap two numbers using a third variable.===================
+
+// let a = 2
+// let b = 3
+//  let temp = a
+//   a = b
+//   b = temp
+//   console.log(a,b)
+
+// ====================4. Swap two numbers without using a third variable.===================
+
+// let a = 10
+// let b = 5
+// a = a + b
+// b = a - b
+// a  = a - b
+// console.log( a, b)
+
+// ================== 5. Find the square of a number.\====================
+
+// function test(n){
+//  for (let i= 1; i<= 10; i++){
+//         console.log(i* i)
+//  }}
+// test(5)
+
+// ===============6. Find the cube of a number.=========================
+
+// function test(){
+// for(let i = 1; i<=10; i++){
+//  console.log(i * i * i)
+// }
+// }
+// test()
+
+//  ======= 7 Convert Celsius to Fahrenheit.=========
+// Formula Of Celsius to Fahrenheit = (celsius * 1.8) + 32
+//  const celsius = 10
+//   const fahrenheit =   (celsius * 1.8)  + 32
+//    console.log(fahrenheit)
+
+//  ======= 8 Convert Fahrenheit to Celsius.============
+//   Formula of Fahrenheit to Celsius =  (Fahrenheit - 32) 1.8
+// const fahrenheit = 50
+//   const celsius = (fahrenheit  - 32) / 1.8
+//   console.log(celsius)
+
+// ========= 9. Calculate simple interest.=============
+//   Formula of Simple Intrest =  (principle * Intrest * time ) / 100
+// const  principle = 5000
+//  const  interestRate = 3
+//  const time = 2
+//    const simpleIntrest = (principle * interestRate * time) / 100
+//    console.log(simpleIntrest)
+
+// ========== 10. Find the area of a circle.========
+//    Formula of area of circle  = πr2
+//  const   circle = 20
+//   const  area = 22/7 * (circle * circle)
+
+//   console.log(area)
+
+/////===============Conditional Statements=====================
+
+//============== 11. Check if a number is even or odd.
+//  function test (n){
+//       if ( n % 2 == 0){
+//         console.log(n,"in is even number")
+//       }
+//       else{
+//          console.log( n , " is  odd number")
+//       }
+//  }
+
+//  test(2)
+//    for(let i = 1; i <=10; i++){
+//     if(i % 2 == 0){
+//         console.log( i , " is even number")
+//     }
+//     else{
+//         console.log(i, "is odd number")
+//     }
+//    }
+
+// ======12 . Check if a number is positive or negative.===========
+//    for ( let i = -5; i<=5; i++){
+//     if(i < 0){
+//         console.log(i,"number is negative")
+//     }
+// else if (i > 0){
+//     console.log(i,"number is positive")
+// }
+//    }
